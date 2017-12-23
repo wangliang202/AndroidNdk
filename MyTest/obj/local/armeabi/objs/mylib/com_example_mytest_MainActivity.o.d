81afe7bf70e9de4b9d3dc4a39137bb4a2f31bcae
wangliang <543390969@qq.com>
2017-12-23 13:54:40 +0800

    android ndk

---
+++ MyTest/obj/local/armeabi/objs/mylib/com_example_mytest_MainActivity.o.d
@@ -0,0 +1,17 @@
+obj/local/armeabi/objs/mylib/com_example_mytest_MainActivity.o: \
+ jni/com_example_mytest_MainActivity.c \
+ jni/com_example_mytest_MainActivity.h \
+ C:/develope/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/jni.h \
+ C:/develope/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/sys/cdefs.h \
+ C:/develope/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/sys/cdefs_elf.h \
+ C:/develope/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/android/api-level.h
+
+jni/com_example_mytest_MainActivity.h:
+
+C:/develope/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/jni.h:
+
+C:/develope/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/sys/cdefs.h:
+
+C:/develope/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/sys/cdefs_elf.h:
+
+C:/develope/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/android/api-level.h:
